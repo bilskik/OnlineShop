9b8f924b0a9d8b9cadd9bf4162ac2a130faf6628
bilskik <kbilski77@gmail.com>
2023-04-12 18:51:23 +0300

    small configs

--- src/main/java/com/bilskik/onlineshop/OnlineShopApplication.java
+++ src/main/java/com/bilskik/onlineshop/OnlineShopApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopApplication.class, args);
+        //cos tam
+    }
 
 }
