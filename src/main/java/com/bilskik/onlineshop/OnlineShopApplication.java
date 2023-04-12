12218833aebc2471b71f9ea1fc264d8ae7a24e67
bilskik <kbilski77@gmail.com>
2023-04-12 18:49:27 +0300

    Initial commit

---
+++ src/main/java/com/bilskik/onlineshop/OnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.bilskik.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopApplication.class, args);
+	}
+
+}
